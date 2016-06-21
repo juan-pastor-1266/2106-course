420b0dfa2990ba6804672b5ed4273e1f25a6475e
juan-pastor-1266 <japf1266@gmail.com>
2016-06-21 18:22:06 +0200

    second commit

--- app/src/main/java/com/example/japf/coursejun16/MainActivity.java
+++ app/src/main/java/com/example/japf/coursejun16/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     TextView textPref;
+    // .................
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
